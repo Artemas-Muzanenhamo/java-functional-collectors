d1283265f0de39f629aaab0f18a541f595b44d47
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-09-21 23:07:00 +0100

    add content on map filter and reduce

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # Java Functional Collectors
+
+## Common Operators
+
+Given some people:
+
+```java
+public static List<Person> createPeople() {
+    return List.of(
+            new Person("Sara", 20),
+            new Person("Sara", 22),
+            new Person("Bob", 20),
+            new Person("Paula", 32),
+            new Person("Paul", 32),
+            new Person("Jack", 3),
+            new Person("Jack", 72),
+            new Person("Jill", 11)
+    );
+}
+```
+
+### filter
+Allows us to pick some values and not pick
+other values within a stream pipeline.
+It takes a `Predicate` which returns a boolean 
+of either `true` or `false` value.
+
+* Get people over the age of 30 only:
+
+```java
+List<Person> over30s = createPeople()
+        .stream()
+        .filter(person -> person.getAge() > 30)
+        .collect(toList());
+```
+
+### map
+This will transform your output in your stream pipeline to be whatever
+type you map your element to in the `.map()` operation.
+
+* Get all people's first names:
+
+```java
+List<String> namesOfOver30s = createPeople()
+        .stream()
+        .map(Person::getName)
+        .collect(toList());
+```
+
+### reduce
+Reduce take the collection and reduces it to a single value.
+Reduce converts a Stream to something more concrete.
+Java has reduce in two forms: 
+- `.reduce()`
+- `.collect()`
+
+* Get the total age of every person:
+
+```java
+Integer totalAgeOfEveryone = createPeople()
+        .stream()
+        .map(Person::getAge)
+        .reduce(0, Integer::sum);
+``` 
---
+++ src/test/java/org/functional/collectors/FilterMapReduceExample.java
@@ -0,0 +1,82 @@
+package org.functional.collectors;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FilterMapReduceExample {
+    public static List<Person> createPeople() {
+        return List.of(
+                new Person("Sara", 20),
+                new Person("Sara", 22),
+                new Person("Bob", 20),
+                new Person("Paula", 32),
+                new Person("Paul", 32),
+                new Person("Jack", 3),
+                new Person("Jack", 72),
+                new Person("Jill", 11)
+        );
+    }
+
+    @Test
+    @DisplayName("Get people over the age of 30")
+    void getOver30s() {
+        List<Person> expectedPeople = List.of(
+                new Person("Paula", 32),
+                new Person("Paul", 32),
+                new Person("Jack", 72)
+        );
+
+        List<Person> over30s = createPeople()
+                .stream()
+                .filter(person -> person.getAge() > 30)
+                .collect(toList());
+
+        assertThat(over30s)
+                .isNotEmpty()
+                .hasSize(3)
+                .containsAnyElementsOf(expectedPeople);
+    }
+
+    @Test
+    @DisplayName("Get the names only")
+    void getNames() {
+        List<String> expectedNames = List.of(
+                "Sara",
+                "Sara",
+                "Bob",
+                "Paula",
+                "Paul",
+                "Jack",
+                "Jack",
+                "Jill"
+        );
+
+        List<String> namesOfOver30s = createPeople()
+                .stream()
+                .map(Person::getName)
+                .collect(toList());
+
+        assertThat(namesOfOver30s)
+                .isNotEmpty()
+                .hasSize(8)
+                .containsAnyElementsOf(expectedNames);
+    }
+
+    @Test
+    @DisplayName("Get the total age of everyone")
+    void getTotalAge() {
+        Integer totalAgeOfEveryone = createPeople()
+                .stream()
+                .map(Person::getAge)
+                .reduce(0, Integer::sum);
+
+        assertThat(totalAgeOfEveryone)
+                .isNotZero()
+                .isEqualTo(212);
+    }
+}
